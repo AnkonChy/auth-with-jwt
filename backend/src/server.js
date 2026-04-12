de29fab2bbd5b779a89ef8317c8359653f3f0573
Ankonchy <ankonchy1@gmail.com>
2026-04-12 22:23:51 +0600

    postgresql connection done and removed .env from remote repo

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,30 @@
+
 import app from "./app.js";
 import dotenv from "dotenv";
+import pool from "./config/db.js";
+
 dotenv.config();
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+
+
+async function startServer() {
+  try {
+    // ✅ Check DB connection
+    await pool.query("SELECT 1");
+    console.log("PostgreSQL connected ✅");
+
+    // ✅ Start server only if DB is OK
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+
+  } catch (err) {
+    console.error("DB connection failed ❌", err);
+  }
+}
+
+startServer();
